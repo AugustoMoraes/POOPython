f8914a7bd2201d2d4dde7606099cb32a15a4bcd9
Augusto Nonato Moraes <devaugusto.moraes@gmail.com>
2023-05-22 21:07:38 -0300

    adicionando modificadores de acesso

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,17 @@
 class Cliente():
     def __init__(self, n, fone):
-        self.nome = n
+        self._nome = n
+        self._telefone = fone
+
+    def get_nome(self):
+        self.nome
+
+    def get_telefone(self):
+        self.telefone
+
+    def set_nome(self, nome):
+        self.nome = nome
+
+    def set_telefone(self, fone):
         self.telefone = fone
+
--- Conta.py
+++ Conta.py
@@ -3,3 +3,35 @@ class Conta:
         self.titular = titular
         self.numero = numero
         self.saldo = 0
+
+    def get_titular(self):
+        self._titular
+
+    def get_numero(self):
+        self._numero
+
+    @property
+    def saldo(self):
+        self._saldo
+
+    @saldo.setter
+    def saldo(self, saldo):
+        if(saldo<0):
+            print("Saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
+    def get_saldo(self):
+        self._saldo
+
+    def set_titular(self, titular):
+        self._titular = titular
+
+    def set_numero(self, numero):
+        self._numero = numero
+
+    def set_saldo(self, saldo):
+        if (saldo<0):
+            print("O saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
+
